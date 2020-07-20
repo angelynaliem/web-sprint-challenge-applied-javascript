fb11e6e6b474534892674eead3a559f501f5fe14
angelynaliem <angelynaliem@gmail.com>
2020-07-19 21:24:19 -0400

    Completed Header.js tasks

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    const headerHeader = document.createElement("div");
+    const spanDateHeader = document.createElement("span");
+    const h1Header = document.createElement("h1");
+    const spanTempHeader = document.createElement("span");
+
+    headerHeader.classList.add("header");
+    spanDateHeader.classList.add("date");
+    spanTempHeader.classList.add("temp");
+
+    spanDateHeader.textContent = "MARCH 28, 2020";
+    h1Header.textContent = "Lambda Times";
+    spanTempHeader.textContent = "98" + '\u00B0';
+
+    headerHeader.append(spanDateHeader, h1Header, spanTempHeader);
+
+    return headerHeader;
+
+}
+
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header());
+
